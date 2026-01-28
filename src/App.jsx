6a1161c6b966e9fa23dd0d2c2af6a7864f446f85
Fatima Nisar <fatimanisar477@gmail.com>
2026-01-28 16:50:57 +0500

    Ems

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Components/Auth/login';
+
+const App = () => {
+  return (
+   <>
+   
+    <Login/>
+    
+   </>
+  )
+}
+
+export default App;
---
+++ src/Components/Auth/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    const[email, setEmail]=useState('');
+    const[password,setpassword]=useState('');
+    const submitHandler =(e)=>{
+        e.preventDefault();
+        console.log("hello guys ");
+    }
+  return (
+    <div className='flex h-screen w-screen  items-center justify-center'>
+        <div className='border-2 border-emerald-600 p-20 rounded-xl'>
+
+            <form className='flex flex-col items-center justify-center'
+             onSubmit={(e)=>{
+                 submitHandler(e);
+                 console.log("Email is",email)
+                 console.log("Password is ",password);
+                 
+                 setEmail('');
+                 setpassword('');
+             }}>
+              
+              <input 
+                    value={email}
+              onChange={(e)=>{
+                  setEmail(e.target.value)
+              }}
+               required className=' text-white outline-none bg-transparent placeholder:text-gray-400  border-2 border-emerald-600 rounded-full  py-3 px-5'  type="email" placeholder='Enter your email'/>
+              <input  
+              value={password}
+              onChange={(e)=>{
+                     setpassword(e.target.value);
+              }}
+               required className=' text-white outline-none bg-transparent placeholder:text-gray-400  border-2 border-emerald-600 rounded-full  py-3 px-5 mt-4' type="password" name="" id="" placeholder='Enter password' />
+              <button className=' text-white border-none outline-none  placeholder:text-white  border-2 bg-emerald-600 rounded-full text-xl py-3 px-4 font-bold  w-64  mt-5 text-xl hover:bg-emerald-700 transition-colors duration-300 '>Log in</button>
+
+
+            </form>
+
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Login;
